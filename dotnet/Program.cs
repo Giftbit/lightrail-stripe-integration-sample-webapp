afc683dba2bfcf1c6d05270d5efe6e7f2deb0bd9
pushplay <pushplay@gmail.com>
2018-01-18 11:43:15 -0800

    Dotnet: .env loading.  Starting on Mustache.

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -13,8 +13,20 @@ namespace dotnet
 {
     public class Program
     {
+        private static readonly string[] RequiredEnvVars = new [] {"LIGHTRAIL_API_KEY", "LIGHTRAIL_SHARED_SECRET", "STRIPE_API_KEY", "STRIPE_PUBLISHABLE_KEY", "TITLE", "SHOPPER_ID", "ORDER_TOTAL"};
+
         public static void Main(string[] args)
         {
+            DotNetEnv.Env.Load(Path.Combine(Directory.GetCurrentDirectory(), "..", "shared", ".env"));
+            foreach (var requiredVar in RequiredEnvVars)
+            {
+                if (System.Environment.GetEnvironmentVariable(requiredVar) == null)
+                {
+                    Console.Error.WriteLine("One or more environment variables necessary to run this demo is/are not set.  See README.md on setting these values.");
+                    return;
+                }
+            }
+
             BuildWebHost(args).Run();
         }
 
